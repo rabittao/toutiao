8137ff00e6b9ccf7371eedf4eecdd4a10da0ef18
wutao <1098571579@qq.com>
2020-11-16 10:35:24 +0800

    第一次版本提交

---
+++ toutiao/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
